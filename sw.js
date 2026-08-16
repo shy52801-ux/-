f848566cd0c5f76b4a5d56a52cc3989b78dd63c3
shy52801-ux <1846302466@qq.com>
2026-08-16 11:43:57 +0800

    sw.js: 网络优先+清理旧缓存+立即接管，修复旧缓存导致的数据错乱

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'weiyang-v2';
+const CACHE_NAME = 'weiyang-v3';
 const urlsToCache = [
     './',
     './index.html',
@@ -14,12 +14,27 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        )).then(() => self.clients.claim())
     );
 });
 
 self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request).then(response => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, copy);
+            }).catch(() => {});
+            return response;
+        }).catch(() => caches.match(event.request))
     );
 });
